cce5859ef49dc7c9bb420a5d30bdf5dbb1104646
DoubleKimp <karstenkeemink@gmail.com>
2022-09-27 11:05:07 +0200

    Added Json functionality
    Co-authored-by: Douwe Zumker <Peredurz@users.noreply.github.com>

--- game.py
+++ game.py
@@ -1,5 +1,38 @@
+import json
+
 
 class game:
-    hp = 100
-    food = 100
+    #print(data["Character"]["hp"]) als we iets uit de jsonfile uit willen printen
+    #data["Character"]["hp"] = 90 #zo pas je dingen aan
     
+    def ReadJson():
+        JsonFile = open("game.json", "r")
+        data = json.load(JsonFile)
+        JsonFile.close()
+        return data
+    
+    def WriteJson(data):
+        with open("game.json", "w") as outfile:
+            json.dump(data, outfile, indent=2)
+        outfile.close()
+    data = ReadJson()
+    
+    #print json data without the curly brackets
+    def PrintInventory(data):
+        print("\nInventory: \n")
+        result = '\n'.join(f'{key}: {value}' for key, value in data["Inventory"].items())
+        print(result)
+    
+    def PrintCharacter(data):
+        print("\nCharacter: \n")
+        result = '\n'.join(f'{key}: {value}' for key, value in data["Character"].items())
+        print(result)
+
+    PrintInventory(data)
+
+    PrintCharacter(data)
+    data["Character"]["hp"] = 100
+    WriteJson(data)
+    
+
+game()
