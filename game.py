83a04c65550ffd98267c1fe9ff8752f43351f18c
DoubleKimp <50771004+DoubleKimp@users.noreply.github.com>
2022-09-28 11:41:34 +0200

    changed the while loop in coherence with douwe

--- game.py
+++ game.py
@@ -54,7 +54,7 @@ class game:
         print("FIGHT: to fight a monster")
         print("TALK: to talk to a character")
         print("PICKUP: to pick up an item")
-        print("EQUIP: to equip an ite")
+        print("EQUIP: to equip an item")
         print("USE: to use an item")
         print("SAVE: to save the game")
         print("EXIT: to exit the game")
@@ -79,63 +79,92 @@ class game:
             game.PrintInventory(data)
             
           
+        game.Pre_Game_Story(data)
+    
+    def Pre_Game_Story(data):
+        print("\nYou suddenly wake up with the worst headache you have ever had.")
+        print("You don't know where you are and you don't know how you got here.")
+        print("You only know that you want to climb the mountain and slay the boss.\n")
         game.room1(data)
     
     def room1(data):
-        print("\nYou suddenly wake up with the worst headache you have ever had.")
-        print("You don't know where you are and you don't know how you got here.")
-        print("You only know that you want to climb the mountain and slay the boss.")
         
-        loop = False
-        while loop == False:
-            command = input("You can write different commands to look around the room (Write HELP for more info)...")
+        print("You can write different commands to look around the room (Write HELP for more info)...")
+        command = input("\nType a command...")
+        while command.lower() != "go door":
+            
+            
             if command.lower() == "help":
                 game.help()
+                command = ""
+                
             elif command.lower() == "inventory":
                 game.PrintInventory(data)
+                command = ""
+                
             elif command.lower() == "character":
                 game.PrintCharacter(data)
+                command = ""
+                
             elif command.lower() == "look":
-                print("You look around the room and notice that it is a nice cozy cabin in the woods.\n"
-                      "You also notice that there is a closet as well as a fridge. There is also a nice\n"
+                print("\nYou look around the room and notice that it is a nice cozy cabin in the woods.\n"
+                      "You also notice that there is a closet as well as a fridge.\nThere is also a nice"
                       "wall with a paint on it.")
+                command = ""
+                
             elif command.lower() == "check closet":
-                print("You check the closet and find some nice and warm winter clothes which you put in your inventory.")
+                print("\nYou check the closet and find some nice and warm winter clothes which you put in your inventory.")
                 data["Inventory"]["WinterClothes"] = 1
-            elif command.lower() == "equip clothes":
-                data["Character"]["WinterClothes"] = 1
                 game.WriteJson(data)
-            elif command.lower() == "go door":
-                if data["Inventory"]["WinterClothes"] == 0:
-                    print("It is cold outside, check if you can find some winterclothes.")
-                elif data["Inventory"]["Shoes"] == 0:
-                    print("Are you not going to wear some shoes when going outside?")
-                else:
-                    loop = True
+                command = ""
+                
             elif command.lower() == "check wall":
-                print("You go to the wall and find some shoes which you stash in your inventory.\n"
+                print("\nYou go to the wall and find some shoes which you stash in your inventory.\n"
                     "You also see a weard painting.")
                 data["Inventory"]["Shoes"] = 1
                 game.WriteJson(data)
-            elif command.lower() == "equip shoes":
-                    data["Character"]["Shoes"] = 1
-                    game.WriteJson(data)
+                command = ""
+                    
             elif command.lower() == "check painting":
-                print("You notice that there is a lever, you can pull it.")
+                print("\nYou notice that there is a lever, you can pull it.")
+                command = ""
+                
             elif command.lower() == "pull lever":
-                print("Pulling the lever opens a secret room.")
+                print("\nPulling the lever opens a secret room.")
                 #functie aanroepen voor die subroom
+                command = ""
+                
             elif command.lower() == "check fridge":
-                print("You check the fridge and find some nice food to take with you on the journey.")
+                print("\nYou check the fridge and find some nice food to take with you on the journey.")
                 data["Inventory"]["Bread"] = 1
                 data["Inventory"]["Water"] = 4
                 data["Inventory"]["Meat"] = 3
                 game.WriteJson(data)
+                command = ""
+                
             else:
-                print("Please enter a valid command")
+                command = input("\nType a valid command... ")
+                
+        
+        if data["Inventory"]["WinterClothes"] == 0 or data["Inventory"]["Shoes"] == 0:
+             print("It is cold outside, check if you can find some clothing and shoes.")
+             
+        else:
+            game.room2(data)
             
         #pass
-
+    def room2(data):
+        print("You are in the room 2")
+        pass
+    
+    def room3(data):
+        pass
+    
+    def room4(data):
+        pass
+    
+    def room5(data):
+        pass
     #PrintInventory(data)
 
     #PrintCharacter(data)
