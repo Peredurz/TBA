5ccd124d0a5f38ec13a433022a6332acf8d4b0cb
Douwe Zumker <douwezumker@gmail.com>
2022-09-30 14:17:25 +0200

    klein begin room9

--- game.py
+++ game.py
@@ -929,6 +929,43 @@ class game:
     def room9(data):
         data["Room"] = 9
         game.WriteJson(data)
+        print("\n You have arrived at a large clifside made of ice.")
+        finish = False
+        while finish == False:
+            if command.lower() == "help":
+                game.help()
+                command = ""
+                
+            elif command.lower() == "inventory":
+                game.PrintInventory(data)
+                command = ""
+                
+            elif command.lower() == "character":
+                game.PrintCharacter(data)
+                command = ""
+            
+            elif command.__contains__("equip") or command.__contains__("EQUIP"):
+                command2 = command.split()
+                game.equip(command2[1], data)
+                command = ""
+            
+            elif command.__contains__("eat") or command.__contains__("EAT"):
+                command2 = command.split()
+                game.food(command2[1], data)
+                command = ""
+
+            elif command.lower() == "save":
+                command = ""
+                game.save(data)
+                
+            
+            elif command.lower() == "exit":
+                print(colored("Exiting game...","red"))
+                exit()
+
+            else:
+                command = input(colored("\n Type a valid command... ", "green"))
+
 
     def room10(data):
         data["Room"] = 10
